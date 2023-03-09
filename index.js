241e69206ff100aed39961b9d74abf715e50d9a9
Zowie Erickson <zerickson@apple.com>
2023-03-08 20:43:43 -0500

    Refactor monster and hero data from const variables to objects. Refactored renderCharacter() function and function call as well

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-const heroElementId = '#hero'
-const heroName = 'Wizard';
-const heroAvatar = 'images/wizard.png'
-const heroHealth = 60;
-const heroDice = 6;
+const hero = {
+    elementId: "#hero",
+    name: "Wizard",
+    avatar: "images/wizard.png",
+    health: 60,
+    diceRoll: 6
+}
 
+const monster = {
+    elementId: "#monster",
+    name: "Orc",
+    avatar: "images/orc.png",
+    health: 10,
+    diceRoll: 4 
+}
 
-const monsterElementId = '#monster';
-const monsterName = 'Orc';
-const monsterAvatar = 'images/orc.png'
-const monsterHealth = 10;
-const monsterDice = 4;
-
-function renderCharacter(elementId, name, avatar, health, dice) {
-    document.querySelector(elementId).innerHTML = `
+function renderCharacter(data) {
+    document.querySelector(data.elementId).innerHTML = `
     <div class="character-card">
-        <h4 class="name"> ${name} </h4>
-        <img class="avatar" src="${avatar}"/>
-        <p class="health">health: <b> ${health} </b></p>
-        <div class="dice-container"><div class="dice"> ${dice} </div></div>
+        <h4 class="name"> ${data.name} </h4>
+        <img class="avatar" src="${data.avatar}"/>
+        <p class="health">health: <b> ${data.health} </b></p>
+        <div class="dice-container"><div class="dice"> ${data.diceRoll} </div></div>
     </div>    
 `
 }
 
-renderCharacter(heroElementId, heroName, heroAvatar, heroHealth, heroDice);
-renderCharacter(monsterElementId, monsterName, monsterAvatar, monsterHealth, monsterDice);
+renderCharacter(hero);
+renderCharacter(monster);
